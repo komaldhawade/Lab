5db0efe63b8f77765dc070e1e9b46dead7843431
komaldhawade <114302894+komaldhawade@users.noreply.github.com>
2023-04-01 22:42:18 +0530

    Update FactorialValue.java

--- 30-03-23/FactorialValue.java
+++ 30-03-23/FactorialValue.java
@@ -1,15 +1,14 @@
 package basic;
 
-public class FactorialValue {
+public class Factorial {
 	
-    for (int i = 1; i <= 10; i++) 
-    {
-        int fact = 1;
-        for (int j = i; j >= 1; j--) 
-        {
-            fact *= j;
-        }
-        System.out.println("Factorial of " + i + " is: " + fact);
-    }
-
-}
+	    public static void main(String[] args) {
+	        for (int i = 1; i <= 10; i++) {
+	            int fact = 1;
+	            for (int j = i; j >= 1; j--) {
+	                fact *= j;
+	            }
+	            System.out.println("Factorial of " + i + " is: " + fact);
+	        }
+	    }
+	}
